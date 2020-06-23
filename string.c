1c113e37e8e4ffac969adb925136930b04eeb7eb
Teofilus Astoputro <tastoputro@internaprima.co.id>
2020-06-23 17:26:08 +0700

    Changed return type of my_strlen

---
+++ string.c
@@ -0,0 +1,15 @@
+int main(void)
+{
+   int i;
+   char *s[] = 
+   {
+      "Git tutorials",
+      "Tutorials Point"
+   };
+
+   for (i = 0; i < 2; ++i)
+      
+   printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+
+   return 0;
+}
